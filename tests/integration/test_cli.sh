beed58222a631bae87249b03a0e9cd8e9c49ba91
Chris Burr <christopher.burr@cern.ch>
2019-12-19 11:55:31 +0100

    Fix shellcheck warnings in tests/integration/test_cli.sh

--- tests/integration/test_cli.sh
+++ tests/integration/test_cli.sh
@@ -8,52 +8,32 @@
 scriptsToTest=(mysql gfal-ls gfal-stat myproxy-info voms-proxy-init2 rrdtool);
 rc=0
 
-for script in ${scriptsToTest[@]};
-do
+for script in "${scriptsToTest[@]}"; do
    # Try --help first
-   ${script} --help &>/dev/null;
-   if [ $? -ne 0 ];
-   then
+   if ! ${script} --help &>/dev/null; then
      # Try just -h
-     ${script} -h &>/dev/null;
-     # If it still fails, try with no options
-     if [ $? -ne 0 ];
-     then
-       ${script}  &>/dev/null;
-       # If it still fails, it fails...
-       if [ $? -ne 0 ];
-       then
-         echo "$script seems not to be working"
+     if ! ${script} -h &>/dev/null; then
+      # If it still fails, try with no options
+       if ! ${script}  &>/dev/null; then
+         # If it still fails, it fails...
          rc=1;
        fi
      fi
    fi
 done
 
-
 # Now some specific tests that do not behave like the other binaries
 
 # For BDDI and ARC
-
-ldapsearch --help 2>&1 >/dev/null | grep -q "usage: ldapsearch";
-
-if [ $? -ne 0 ];
-then
+if ! (ldapsearch --help 2>&1 >/dev/null | grep -q "usage: ldapsearch"); then
   echo "ldapsearch not working";
   rc=1;
 fi
 
-
 # For SSHComputingElement
-
-ssh --help 2>&1 >/dev/null | grep -q "usage:";
-
-if [ $? -ne 0 ];
-then
+if ! (ssh --help 2>&1 >/dev/null | grep -q "usage:"); then
   echo "ssh not working";
   rc=1;
 fi
-
-
 
 exit $rc
